eeb391f8b68683b8a8a30a21f0802c26c12a30d8
Avinash Dhauni <avinashdhauni96@gmail.com>
2022-08-18 15:41:41 +0530

    complete tip calculator challenge

---
+++ script.js
@@ -0,0 +1,50 @@
+const billInput = document.querySelector("#billInput");
+const tip = document.querySelectorAll(".tip");
+const totalTip = document.getElementById("totalTip");
+const custom = document.getElementById("custom");
+const numberOfPeople = document.getElementById("people-input");
+const totalAmount = document.querySelector("#totalAmount");
+
+let tipValue = 0;
+let people = 1;
+let tipPercent = 0;
+
+tip.forEach(function(val){
+    val.addEventListener("click", calculateTip);
+})
+
+function calculateTip(event){
+    if(custom.value !== ""){
+        tipPercent = Number(custom.value);
+    }else{
+        tipPercent = (event.target.innerHTML).replace("%", "");
+    }
+    tipValue = (tipPercent/100) * billInput.value;
+    totalTip.innerHTML = tipValue;
+    totalAmt();
+}
+
+custom.addEventListener("input", calculateTip);
+
+billInput.addEventListener("input", totalAmt);
+
+numberOfPeople.addEventListener("input", totalAmt);
+
+function totalAmt(){
+    people = Number(numberOfPeople.value);
+
+    if(numberOfPeople.value === ""){
+        people = 1;
+    }
+    let total = (Number(billInput.value) + tipValue)/people
+    totalAmount.innerHTML = "$" +total;
+}
+
+function reset(){
+    billInput.value = ""
+    custom.value = ""
+    numberOfPeople.value = ""
+    
+    totalTip.innerHTML = "$0.00";
+    totalAmount.innerHTML = "$0.00";
+}
